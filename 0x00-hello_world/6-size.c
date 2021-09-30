8c6bc474c8479e7fc22a5ee797f104eccea04272
Jhon Bueno <jhon@monbu.co>
2021-09-30 20:49:12 +0000

    Sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %s\n", sizeof(c));
-	printf("Size of an int: %d\n", sizeof(i));
-	printf("Size of a long int: %li\n", sizeof(li));
-	printf("Size of a long long int: %lli\n", sizeof(lli));
-	printf("Size of a float: %f", sizeof(f));
+	printf("Size of a char: %lu\n", sizeof(c));
+	printf("Size of an int: %lu\n", sizeof(i));
+	printf("Size of a long int: %lu\n", sizeof(li));
+	printf("Size of a long long int: %lu\n", sizeof(lli));
+	printf("Size of a float: %lu", sizeof(f));
 	return (0);
 }

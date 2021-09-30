b11eedd554077dadff5bd560c58748b5149320d9
Jhon Bueno <jhon@monbu.co>
2021-09-30 21:40:37 +0000

    print

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print quote
+ * Return: return 1
+ */
+
+int main(void)
+{
+	print("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}

e2fd65e61f48ca65f3e003a83e72955fb727698a
Jhon Bueno <jhon@monbu.co>
2021-10-01 19:15:51 +0000

    Change switch by if, else if

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -25,16 +25,16 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-		printf("Last digit of %d and is greater than 5\n", last_digit);
+		printf("Last digit of %d and is greater than 5\n", n);
 	}
 
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d and is 0\n", last_digit);
+		printf("Last digit of %d and is 0\n", n);
 	}
 	else if (last_digit < 6 && last_digit != 0)
 	{
-		printf("Last digit of %d and is less than 6 and not 0\n", last_digit);
+		printf("Last digit of %d and is less than 6 and not 0\n", n);
 	}
 
 	return (0);

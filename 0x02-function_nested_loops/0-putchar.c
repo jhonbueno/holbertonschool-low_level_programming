1def32871e0df26b1f008bee6745cd5ec0982820
Jhon Bueno <jhon@monbu.co>
2021-10-04 22:17:42 +0000

    Putchar

--- 0x02-function_nested_loops/0-putchar.c
+++ 0x02-function_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "_putchar.c"
+#include "main.h"
 
 
 /**
@@ -9,7 +9,15 @@
 
 int main(void)
 {
-	_putchar("_putchar");
-	
+	char put[] = "_putchar";
+	int i;
+
+	for (i = 0; i <= 7; i++)
+	{
+		_putchar(put[i]);
+	}
+
+	_putchar('\n');
+
 	return (0);
 }

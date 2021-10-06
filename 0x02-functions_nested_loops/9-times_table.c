934f91cad436fc4e861011ea21d5f324a799d77c
Jhon Bueno <jhon@monbu.co>
2021-10-06 15:59:59 +0000

    9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,67 +9,33 @@
 
 void times_table(void)
 {
-	int r, c, i, j, m, d1, d2;
+	int r, c, i, m, d1, d2;
 
 	for (r = 0; r <= 9; r++)
 	{
 		for (c = 0; c <= 9; c++)
 		{
 			if (r == 0)
-			{
 				_putchar('0');
-
-				if (c < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
-				else if (c == 9)
-					_putchar('$');
-
-				_putchar('\n');
-			}
-
 			else if (r == 1)
-			{
-				for (i = 0; i <= 9; i++)
-				{
-					_putchar((i) + '0');
-
-					if (i < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (i == 9)
-						_putchar('$');
-				}
-				_putchar('\n');
-			}
+				_putchar(c + '0');
 			else
 			{
-				for (j = 1; j <= 9; j++)
-				{
-					m = r * j;
+				m = r * c;
+				d1 = m / 10;
+				d2 = m % 10;
 
-					d1 = m / 10;
-
-					d2 = m % 10;
-
-					_putchar(d1 + '0');
-					_putchar(d2 + '0');
-
-					if (c < 9)
-					{
-						_putchar(',');
-						_putchar(' ');
-					}
-					else if (c == 9)
-						_putchar('$');
-					_putchar('\n');
+				_putchar(d1 + '0');
+				_putchar(d2 + '0');
 				}
 			}
-
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (c == 9)
+				_putchar('$');
 
 		}
 

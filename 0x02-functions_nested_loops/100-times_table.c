c55e738a0654244861e8c761d1e9cc4099162364
Jhon Bueno <jhon@monbu.co>
2021-10-06 20:30:13 +0000

    100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * print_times_table - Print n times table
+ *
+ *
+ * Return: 0
+ */
+
+void print_times_table(int n)
+{
+	int r, c, m, d1, d2;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (r = 0; r <= n; r++)
+		{
+			for (c = 0; c <= n; c++)
+			{
+				m = r * c;
+				d1 = m / 10;
+				d2 = m % 10;
+
+				if (m == d2)
+					_putchar(d2 + '0');
+
+				else
+				{
+					_putchar(d1 + '0');
+					_putchar(d2 + '0');
+				}
+				if (c < n)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					if ((r * (c + 1)) < 10)
+						_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}

c4ca6ddc38e56d0327b2f39fce64e5fa6534c3b6
Jhon Bueno <jhon@monbu.co>
2021-10-06 16:04:34 +0000

    9 table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,20 +15,13 @@ void times_table(void)
 	{
 		for (c = 0; c <= 9; c++)
 		{
-			if (r == 0)
-				_putchar('0');
-			else if (r == 1)
-				_putchar(c + '0');
-			else
-			{
-				m = r * c;
-				d1 = m / 10;
-				d2 = m % 10;
+			m = r * c;
+			d1 = m / 10;
+			d2 = m % 10;
 
-				_putchar(d1 + '0');
-				_putchar(d2 + '0');
-				}
-			}
+			_putchar(d1 + '0');
+			_putchar(d2 + '0');
+
 			if (c < 9)
 			{
 				_putchar(',');

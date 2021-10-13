111dcc668c85c74621ca5521953ab0be9cbd654f
Jhon Bueno <jhon@monbu.co>
2021-10-13 01:47:30 +0000

    Strcpy

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i, j;
-	char *dest[1000];
+	char dest[1000];
 
 	i = 0;
 
 	while(src[i] != '\0')
 		i++;
-	for (j = 0; i <= i, j++)
+	for (j = 0; j <= i, j++)
 		dest[j] = src[j];
 
 	return (*dest);

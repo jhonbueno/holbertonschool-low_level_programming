c6e94f948687bd5902a663862e5ba52e668c0793
Jhon Bueno <jhon@monbu.co>
2021-10-13 01:17:33 +0000

    Print n elements

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,16 +12,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	i = 0;
-
 	for (i = 0; i <= n; i++)
 	{
-		_putchar(a[i]);
+		printf("%d", a[i]);
 		if (i < n)
-		{
-			_putchar(',');
-			_putchar(' ');
-		}
+			printf(", ");
 	}
-	_putchar('\n');
+	printf("\n");
 }

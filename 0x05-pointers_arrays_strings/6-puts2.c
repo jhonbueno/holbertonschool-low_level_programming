fea71a105c6bbe3e1ad922d1c4eb7ea9fc95d81e
Jhon Bueno <jhon@monbu.co>
2021-10-13 00:26:45 +0000

    Other characters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,14 +9,15 @@
 
 void puts2(char *str)
 {
-	int i;
+	int i, j;
 
-	i = 1;
+	i = 0;
 
 	while (str[i] != '\0')
 	{
-		_putchar(str[i]);
-		i += 2;
+		i++;
 	}
+	for (j = 0; j <= i; j += 2)
+		_putchar(str[j]);
 	_putchar('\n');
 }

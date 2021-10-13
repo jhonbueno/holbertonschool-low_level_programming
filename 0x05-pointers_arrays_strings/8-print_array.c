0469cb5fc94abf1c1c06469fabecd884b2cf5799
Jhon Bueno <jhon@monbu.co>
2021-10-13 01:07:31 +0000

    Print n elements

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print string
+ * @str: Char
+ *
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int i, j;
+
+	i = 0;
+
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(a[i]);
+		if (i < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+}

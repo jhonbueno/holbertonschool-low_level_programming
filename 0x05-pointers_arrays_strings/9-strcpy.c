0c87c6850dc1fea833691200d715b2d047d859f1
Jhon Bueno <jhon@monbu.co>
2021-10-13 01:30:42 +0000

    Strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - print string
+ * @dest: char
+ * @src: char
+ *
+ * Return: char
+ */
+
+char _strcpy(char *dest, char *src)
+{
+
+	*dest = *src;
+
+	return (*dest);
+}

7a8f9deb20d1af315aca42dd71733fbe1d6ff68e
Jhon Bueno <jhon@monbu.co>
2021-10-13 18:03:19 +0000

    Concatenate string

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strcat - print string
+ * @dest: char
+ * @src: char
+ *
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+	k = 0;
+
+	while (src[i] != '\0')
+		i++;
+
+	while (dest[j] != '\0')
+		j++;
+
+	while (k <= i)
+	{
+		dest[j+k] = src[k];
+		k++;
+	}
+
+	return (dest);
+}

960217676f1e235de2d0d21a110d302d374948d7
Jhon Bueno <jhon@monbu.co>
2021-10-14 17:59:30 +0000

    Update

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,21 +4,27 @@
  * _strncat - print string
  * @dest: char
  * @src: char
- * @n: size
+ * @n: n
  *
  * Return: char
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int j, k;
+	int i, j, k;
 
+	i = 0;
 	j = 0;
 	k = 0;
+
+	while (src[i] != '\0')
+		i++;
 
 	while (dest[j] != '\0')
 		j++;
 
+	if (n > i)
+		n = i;
 	while (k <= n)
 	{
 		dest[j + k] = src[k];

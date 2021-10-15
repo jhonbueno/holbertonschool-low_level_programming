9eb5c98b3e8e39ad8174680687722155dbab00e3
Jhon Bueno <jhon@monbu.co>
2021-10-15 23:09:30 +0000

    To upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * string_toupper - print string
+ * @str: char
+ *
+ *
+ *
+ * Return: char
+ */
+
+char *string_toupper(char *str);
+{
+	int i, j, k;
+
+	i = 0;
+	j = 0;
+
+	while(str[i] != '\0')
+		i++;
+
+	for (j = 0; j <= i; j++)
+	{
+		if (str[j] >= 'a' && str[j] <= 'z')
+			str[j] = toupper(str[j]);
+	}
+	return (str);
+}

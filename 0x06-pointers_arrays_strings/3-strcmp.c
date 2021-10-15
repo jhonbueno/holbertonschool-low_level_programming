1874856bc50c9adae44b5dd40204470419afa852
Jhon Bueno <jhon@monbu.co>
2021-10-15 20:29:42 +0000

    3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,13 @@
 
 int *_strcmp(char *s1, char *s2)
 {
-	int i, j, k, l;
+	int i, j, k, l, r;
 
         i = 0;
         j = 0;
         k = 0;
         l = 0;
+	r = 0;
 
         while(s1[i] != '\0')
 		i++;
@@ -28,7 +29,11 @@ int *_strcmp(char *s1, char *s2)
         for (l = 0; l < k; l++)
         {
 		if (s1[l] != s2[l])
-			return (s1[l] - s2[l]);
+		{
+			r = s1[l] - s2[l];
+			return (r);
+		}
         }
-        return (s1[l] - s2[l]);
+	r = s1[l] - s2[l];
+	return (r);
 }

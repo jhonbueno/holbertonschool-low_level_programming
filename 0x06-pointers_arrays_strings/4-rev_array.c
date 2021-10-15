bc97f0558b2e563cb26f62ba73511e70fcdcb8bd
Jhon Bueno <jhon@monbu.co>
2021-10-15 22:43:49 +0000

    4 update

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -12,17 +12,17 @@
 void reverse_array(int *a, int n)
 {
 	int i, j, k;
-	char a2[n];
+	int a2[n];
 
 	j = 0;
 
-	for (i = n; i >= 0; i--)
+	for (i = n -1; i >= 0; i--)
 	{
 		a2[j] = a[i];
 		j++;
 	}
 
-	for (k = 0; k < n; k++)
+	for (k = 0; k <= n; k++)
 	{
 		a[k] = a2[k];
 	}

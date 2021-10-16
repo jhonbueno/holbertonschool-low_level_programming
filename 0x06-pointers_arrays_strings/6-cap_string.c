377f6a4a5c64080c777378360c0f45c9cba93dfa
Jhon Bueno <jhon@monbu.co>
2021-10-16 01:57:40 +0000

    Fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -23,6 +23,9 @@ char *cap_string(char *str)
 		i++;
 	for (j = 0; j < i; j++)
 	{
+		str[0] = toupper(str[0]);
+		if (str[j] >= 'A' && str[j + 1] <= 'Z' && j > 0)
+			str[j] = tolower(str[j]);
 		for (k = 0 ;  k < 12 ; k++)
 		{
 			if (str[j] == sep[k])

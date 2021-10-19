b56b7825b66e91214af05a43d086061e79107219
Jhon Bueno <jhon@monbu.co>
2021-10-19 12:35:27 +0000

    Update 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,25 +10,32 @@
 
 unsigned int _strspn(char *s, char *accept)
 {
-	int i, j, k, l;
+	int i, j, k, l, flag;
 	unsigned int count;
 
 	i = 0;
 	l = 0;
 	count = 0;
+	flag = 1;
 
 	while (accept[i] != '\0')
 		i++;
 	while (s[l] != '\0')
 		l++;
 
-	for (j = 0; j < l; j++)
+	for (j = 0; j < l && flag == 1; j++)
 	{
 		for (k = 0; k <= i; k++)
 		{
-			if (accept[k] == s[j])
+			if (S[j] == accept[k])
+			{
 				count++;
+				flag =1;
+				break;
+			}
 		}
+		else
+			flag = 0;
 	}
 	return (count);
 }

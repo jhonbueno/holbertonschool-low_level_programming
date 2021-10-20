3b9da78469886291dc6ab3a822d1f944f4241974
Jhon Bueno <jhon@monbu.co>
2021-10-20 19:17:29 +0000

    1 print rev

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - puts reverse
+ * @s: char
+ *
+ *
+ * Return: 0
+ */
+
+void _print_rev__recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	else
+	{
+		_puts_recursion(s + 1);
+		putchar(*s);
+	}
+}

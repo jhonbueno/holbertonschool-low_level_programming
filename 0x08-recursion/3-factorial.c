fe32945c4431c1f7799658af66c78e42039d8b4c
Jhon Bueno <jhon@monbu.co>
2021-10-20 20:29:38 +0000

    3 factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * factorial - puts reverse
+ * @n: int
+ *
+ *
+ * Return: integer
+ */
+
+int factorial(int n)
+{
+	int fact = 1;
+
+	if (n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		fact = n * factorial(n - 1);
+		return (fact);
+	}
+}

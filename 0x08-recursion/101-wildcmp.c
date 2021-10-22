011a80eee6e1d5ef9a2369ab5078fad8bf376272
Jhon Bueno <jhon@monbu.co>
2021-10-22 18:40:03 +0000

    Update 101

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -23,11 +23,11 @@ int wildcmp(char *s1, char *s2)
 	{
 		return (wildcmp(s1, s2 + 1));
 	}
-	if (*s1 != *s2 && *(s2 - 1) != '*')
+	if (*s1 != *s2 && *(s1 - 1) == *(s2 - 1))
 	{
 		return (0);
 	}
-	else if (*s1 != *s2 && *s1 != '\0')
+	if (*s1 != *s2 && *s1 != '\0')
 	{
 		return (wildcmp(s1 + 1, s2));
 	}

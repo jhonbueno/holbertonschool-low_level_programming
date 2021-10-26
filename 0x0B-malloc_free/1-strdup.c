af36a0073be25cc88eaf8f9a15a55cfd3a4e085b
Jhon Bueno <jhon@monbu.co>
2021-10-26 22:21:57 +0000

    Update 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -15,19 +15,18 @@ char *_strdup(char *str)
 	char *s;
 	int len, i;
 
-	len = strlen(str);
-
-	s = malloc(sizeof(char) * len);
-
-	if (s == NULL || str == NULL)
+	if (str == NULL)
 		return (NULL);
 
 	len = strlen(str);
 
+	s = malloc(sizeof(char) * len);
+
+	if (s == NULL)
+		return (NULL);
+
 	for (i = 0; i < len; i++)
 		s[i] = str[i];
 
-	i++;
-	s[i] = '\0';
 	return (s);
 }

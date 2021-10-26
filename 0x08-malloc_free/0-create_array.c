55cf54ba87428b2d811c38de0c4c2b0cc307c012
Jhon Bueno <jhon@monbu.co>
2021-10-26 19:15:29 +0000

    Malloc 0

---
+++ 0x08-malloc_free/0-create_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - create array malloc
+ * @size: unsigned int
+ * @c: char
+ *
+ * Return: pointer
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	s = malloc(sizeof(char) * size);
+
+	if (size == 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+		s[i] = c;
+}

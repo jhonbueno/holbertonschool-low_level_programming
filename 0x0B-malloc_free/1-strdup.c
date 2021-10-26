57708066532949527a16cb3110ae1123a811eecf
Jhon Bueno <jhon@monbu.co>
2021-10-26 21:31:36 +0000

    Copy string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _strdup - copy string
+ * @str: char
+ *
+ *
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *s;
+	int len, i;
+
+	len = strlen(str);
+
+	s = malloc(sizeof(char) * len);
+
+	if (s == NULL || str == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
+
+	return (s);
+}

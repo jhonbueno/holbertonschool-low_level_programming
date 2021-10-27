0035f7026e04016796de53a4a63223213dce36b9
Jhon Bueno <jhon@monbu.co>
2021-10-27 00:17:32 +0000

    2 concatenate

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -27,6 +27,8 @@ char *str_concat(char *s1, char *s2)
 		s1[0] = '\0';
 	if (s2 == NULL)
 		s2[0] = '\0';
+	if (s1 == NULL && s2 == NULL)
+		s[0] = '\0';
 	for (i = 0; i < len1; i++)
 	{
 		s[i] = s1[i];

846a0745f969e62623078f8e88de4b6bd06152ae
Jhon Bueno <jhon@monbu.co>
2021-10-29 19:08:27 +0000

    2 calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -22,7 +22,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < (size * nmemb); i++)
 		s[i] = 0;
 
 	return (s);

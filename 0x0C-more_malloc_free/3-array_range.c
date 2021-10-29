3cc33a9838923b3c62f52750722ba32d3af922c0
Jhon Bueno <jhon@monbu.co>
2021-10-29 19:53:20 +0000

    Create array

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -14,15 +14,16 @@
 int *array_range(int min, int max)
 {
 	int *s;
-	int i;
+	int i, dist;
 
 	if (min > max)
 		return (NULL);
+	dist = max - min;
 	s = malloc(sizeof(int) * (max - min));
 	if (s == NULL)
 		return (NULL);
 
-	for (i = 0; i < (max - min); i++)
+	for (i = 0; i < dist; i++)
 	{
 		s[i] = min;
 		min++;

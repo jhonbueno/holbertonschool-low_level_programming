ced28fafcbebd8906d6b5818c155026a27e4d87e
Jhon Bueno <jhon@monbu.co>
2021-11-01 21:38:21 +0000

    Dog initialize

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+/**
+ * init_dog - initialize dog
+ * @d: dog
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * Return: 0
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owoner = owner;
+
+	return (0);
+}

ee5f56a5650c3056f97cf19c9730a71e6d02d9b2
Jhon Bueno <jhon@monbu.co>
2021-11-02 18:07:06 +0000

    Update

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - print dog
+ * @d: dog type
+ *
+ *
+ *
+ * Return: 0
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d);
+
+}

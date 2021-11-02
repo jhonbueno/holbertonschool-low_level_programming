8b3c49b2c191fe6b25196b536675942a09d8b6ff
Jhon Bueno <jhon@monbu.co>
2021-11-02 04:32:24 +0000

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,18 +16,21 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog1;
+	unsigned int i, j;
 
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return (NULL);
-	dog1->name = malloc(sizeof(dog_t));
+	dog1->name = malloc(sizeof(char) * strlen(name) + 1);
 	if (dog1->name == NULL)
 		return (NULL);
-	dog1->name = name;
+	for (i = 0; i < strlen(name); i++)
+		dog1->name[i] = name[i];
 	dog1->age = age;
-	dog1->owner = malloc(sizeof(dog_t));
+	dog1->owner = malloc(sizeof(char) * strlen(owner) + 1);
 	if (dog1->owner == NULL)
 		return (NULL);
-	dog1->owner = owner;
+	for (j = 0; j < strlen(owner); j++)
+		dog1->owner[j] = owner[j];
 	return (dog1);
 }

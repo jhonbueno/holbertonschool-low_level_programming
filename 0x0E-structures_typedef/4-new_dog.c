a1f970a13b8d805d71da12747a1d9b638838b5ef
Jhon Bueno <jhon@monbu.co>
2021-11-02 03:34:22 +0000

    Update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 /**
  * new_dog - print dog
@@ -19,8 +20,14 @@ dog_t *new_dog(char *name, float age, char *owner)
 	dog1 = malloc(sizeof(dog_t));
 	if (dog1 == NULL)
 		return (NULL);
+	dog1->name = malloc(sizeof(char) * strlen(name) + 1);
+	if (dog1->name == NULL)
+		return (NULL);
 	dog1->name = name;
 	dog1->age = age;
+	dog1->owner = malloc(sizeof(char) * strlen(owner) + 1);
+	if (dog1->owner == NULL)
+		return (NULL);
 	dog1->owner = owner;
 	return (dog1);
 }

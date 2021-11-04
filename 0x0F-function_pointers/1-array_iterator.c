458d73aa3e17bee1c0330e3bcbe46a7a9a3ddcb6
Jhon Bueno <jhon@monbu.co>
2021-11-04 21:27:35 +0000

    Iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+/**
+ * array_iterator - Array iterator
+ * @array: array
+ * @size: size array
+ * @action: pointer to function
+ *
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array && action)
+	{
+		size_t i;
+
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}

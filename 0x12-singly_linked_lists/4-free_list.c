39f704cb22ba1cc9f51bdd752a831de9271edf37
Jhon Bueno <jhon@monbu.co>
2021-11-13 02:28:23 +0000

    Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include "lists.h"
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ * add_node_end - add node end
+ * @head: list_t list
+ * @str: string
+ *
+ *
+ *
+ * Return: size_t
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node, *current;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	new_node->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+	} else
+	{
+		current = *head;
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (new_node);
+}

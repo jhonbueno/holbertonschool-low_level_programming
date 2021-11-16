a71c0aeb538b1f23bea2d8da9daa3c06f8874a9e
Jhon Bueno <jhon@monbu.co>
2021-11-16 23:18:46 +0000

    Update

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -17,7 +17,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 	list_t *new_node, *current;
 
-	new_node = malloc(sizeof(list_t));
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 	{
 		free(new_node);

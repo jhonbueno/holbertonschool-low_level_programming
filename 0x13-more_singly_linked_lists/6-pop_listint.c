214db5b7a9d7d2b104c554d0d9b0d17aef45abab
Jhon Bueno <jhon@monbu.co>
2021-11-17 15:06:02 +0000

    Pop head

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * pop_listint - free a list
+ * @head: pointer to list
+ *
+ * Return: void
+ */
+
+int pop_listint(listint_t **head)
+{
+	int retval = -1;
+	listint_t *next_node = NULL;
+
+	if (*head == NULL)
+		return (-1);
+
+	next_node = (*head)->next;
+	retval = (*head)->n;
+	free(*head);
+	*head = next_node;
+
+	return (retval);
+}

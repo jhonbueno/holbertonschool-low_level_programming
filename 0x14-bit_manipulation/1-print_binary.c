468378881c62670d5e0f9ecff7d98b800a4c81b7
Jhon Bueno <jhon@monbu.co>
2021-11-19 22:26:08 +0000

    Update

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,42 +1,27 @@
 #include "main.h"
 
-/**
- * recursive_binary - recursive binary calculus
- * @n: The number given
- * @b: initial state of n
- *
- * Return: none
- *
- */
-
-void recursive_binary(unsigned long int n, unsigned long int b)
-{
-	unsigned long int bin = 0;
-
-	if (b == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if (n == 0)
-		return;
-
-	bin = n % 2;
-	recursive_binary(n / 2, b);
-	_putchar(bin + '0');
-}
-
-/**
- * print_binary - print binary
- *
- * @n: integer number
- *
- * Return: unsigned integer
- */
-
 void print_binary(unsigned long int n)
 {
 	unsigned long int b = n;
+	int i = 0;
 
-	recursive_binary(n, b);
+	if (n == 0)
+		_putchar('0');
+
+	while (b > 0)
+	{
+		b >>= 1;
+		i++;
+	}
+
+	i--;
+
+	while (i >= 0)
+	{
+		if (1 & (n >> i))
+			_putchar('1');
+		else
+			_putchar('0');
+		i--;
+	}
 }

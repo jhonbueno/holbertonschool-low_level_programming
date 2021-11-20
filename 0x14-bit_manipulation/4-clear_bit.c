f9367fd54ff20836fda2b695dfef15c6feb90362
Jhon Bueno <jhon@monbu.co>
2021-11-20 01:42:32 +0000

    Set 0

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -15,7 +15,7 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	if (index > (sizeof(unsigned long int) * 8))
 		return (-1);
 
-	*n = (0 << index) | *n;
+	*n = ~(1 << index) & *n;
 
 	return (1);
 }

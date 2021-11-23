f95e4db70b5c9675f95be387af88d92fc8f61b0f
Jhon Bueno <jhon@monbu.co>
2021-11-23 14:50:36 +0000

    Update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -45,7 +45,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		buf[nr_bytes] = '\0';
 		text = write(STDOUT_FILENO, buf, nr_bytes);
 		close(fd);
-		if (text == -1)
+		if (text == -1 || text != nr_bytes)
 		{
 			free(buf);
 			return (0);

a45e385d13e6cd9c68961335ffd085cec52b6999
Jhon Bueno <jhon@monbu.co>
2021-11-23 18:59:13 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -24,16 +24,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = creat(filename, 0600);
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
 	if (fd == -1)
 	{
+		close(fd);
 		return (-1);
 	}
 
 	else
 	{
-		if (!text_content)
+		if (text_content == NULL)
 		{
 			close(fd);
 			return (1);

18c8363159184e9254ef79f1a846dd3db82205bf
Jhon Bueno <jhon@monbu.co>
2021-11-23 18:49:27 +0000

    Update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,9 +12,9 @@
  * create_file - Create file
  *
  * @filename: filename
- * @letters: number of letters to print
+ * @text_content: content
  *
- * Return: ssize_t
+ * Return: int
  */
 
 int create_file(const char *filename, char *text_content)
@@ -33,6 +33,11 @@ int create_file(const char *filename, char *text_content)
 
 	else
 	{
+		if (!text_content)
+		{
+			close(fd);
+			return (1);
+		}
 		text = write(STDOUT_FILENO, text_content, strlen(text_content));
 		close(fd);
 
